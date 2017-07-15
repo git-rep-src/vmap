49498fa1e878b4102a795d562746436d601f9cd8
git-rep <git-rep@openmailbox.org>
2017-07-14 21:09:33 -0300

    Add check open file.

--- src/main.cc
+++ src/main.cc
@@ -8,11 +8,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     
     QFile file(":/style-default");
-    file.open(QFile::ReadOnly);
-    QString stylesheet = QLatin1String(file.readAll());
-    file.close();
-    qApp->setStyleSheet(stylesheet);
-    
+    if (file.open(QFile::ReadOnly)) {
+        QString stylesheet = QLatin1String(file.readAll());
+        file.close();
+        qApp->setStyleSheet(stylesheet);
+    }
+
     Vmap vmap;
     vmap.showFullScreen();
 
